48fa5865071bf5aedce9a97ee797ad68d3b3c88a
masterit15 <masterit15@yandex.ru>
2021-02-10 13:36:56 +0300

    new

--- app/components/bitrix/catalog/adm_reg/sections.php
+++ app/components/bitrix/catalog/adm_reg/sections.php
@@ -63,16 +63,14 @@ usort($arStruktura, sortStruktura);
 
 ?>
 <div class="content">
+	<div class="row">
 	  <p class="col-sm-3"><small>Наименование услуги</small></p>
 	  <p class="col-sm-3"><small><nobr>Исполнитель услуги</nobr> <nobr>(разработчик регламента)</nobr></small></p>
 	  <p class="col-sm-2"><small><nobr>Дата размещения</nobr> проекта регламента на сайте</small></p>
 	  <p class="col-sm-2"><small><nobr>Срок проведения</nobr> независимой экпертизы</small></p>
 	  <p class="col-sm-2"><small>Статус</small></p>
-	<table class="regl-table" cellpadding="0" cellspacing="0" height="14" width="100%">
-	<tbody>
-	<tr><td colspan="4"><td></tr>
-	</tbody>  
-</table> 
+	</div>
+	
 <?
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 $arResult = $arStruktura;
@@ -84,7 +82,7 @@ if (!empty($arResult)){
 //ЗАГОЛОВОК ------------------
 //   Вывод информации о СФЕРЕ услуг -----------------------------------------------------------------------------------------------------------------------------------------------------------
 ?>
-<div><p class="static_text"><?=$arItem['NAME'];?></p></div>
+<h3 class="static_text"><?=$arItem['NAME'];?></h3>
 <?
 
 	if	(!$arItem['SECTION']){
@@ -103,11 +101,6 @@ if (!empty($arResult)){
 			if($arOfficer['NAME'] != "")
 				$arItem['NAME'] = $arOfficer['NAME'];
 		}
-		if($arItem['NAME'] != " " && $arItem['NAME'] != "-" && $arItem['NAME'] != "_"){
-//					$sub_str.= "
-//	<tr><td align=\"left\" valign=\"bottom\" height=\"4\" style=\"padding: 2px 10px 0px ".(7+(15*$l))."px;\"><li><a href=\"?IBLOCK_ID=".$arParams['IBLOCK_ID']."&SECTION_ID=0&ELEMENT_ID=".$arItem['ID']."\" class=\"submenu_l3\">".$arItem['NAME']."</a></li></td></tr>
-//	<tr><td align=\"left\" valign=\"bottom\" height=\"5\" background=\"/bitrix/templates/AMS_Vladikavkaz/images/submenu_divfill.gif\"><img src=\"/bitrix/templates/AMS_Vladikavkaz/images/submenu_divdot.gif\" width=\"5\" height=\"5\"></td></tr>";
-		}
 	}elseif	($arItem['NAME'] != " " && $arItem['NAME'] != "-" && $arItem['NAME'] != "_"){
 		$arSelect = Array("ID", "NAME", "SORT");
 		$arFilter = Array(	"IBLOCK_TYPE"	=>	$arParams["IBLOCK_TYPE"],
@@ -124,23 +117,13 @@ if (!empty($arResult)){
 			if($arFields['NAME'] != " " && $arFields['NAME'] != "-" && $arFields['NAME'] != "_"){
 
 ?>
-<div class="doc-item col-sm-12">
+<div class="table_row row">
 <?
 //   Вывод информации об УСЛУГЕ ---------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-//print_r($arFields);
-
-//	Наименование услуги	- $arFields['NAME']
-//	Дата размещения		- $arFields['ACTIVE_FROM']
-//	Общие сведения		- $arFields['DETAIL_TEXT']
-
 	$arPropertyD = Array();
 	$rsProperties = CIBlockElement::GetProperty($arParams["IBLOCK_ID"], $arFields["ID"], "SORT", "ASC", array("ACTIVE"=>"Y"));
 	while($arProperty = $rsProperties->Fetch())
 	{
-
-//print_r($arProperty);
-
 		if($arProperty['PROPERTY_TYPE'] != "F")
 		{
 			$arPropertyD[$arProperty['CODE']] = Array
@@ -151,95 +134,28 @@ if (!empty($arResult)){
 			);
 		}
 	}
-//   Вывод ДОПОЛНИТЕЛЬНОЙ информации об услуге ------------------------------------------------------------------------------------------------------------------------------------------------
-
-//	Статус документа	- $arPropertyD['rmu_status']
-//	Исполнитель услуги	- $arPropertyD['rmu_ispolnitel']
-//	Контактная информация	- $arPropertyD['rmu_inform']
-
-//print_r($arPropertyD);
-
-/*
-	$rsProperties = CIBlockElement::GetProperty($arParams["IBLOCK_ID"], $arFields["ID"], "SORT", "ASC", array("ACTIVE"=>"Y","PROPERTY_TYPE"=>"F"));
-	while($arProperty = $rsProperties->Fetch())
-	{
-
-//ПРИКРЕПЛЕННЫЕ ФАЙЛЫ -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//print_r($arProperty);
-		if ($arProperty["VALUE"] && $arProperty["NAME"]){
-			$rsFile = CFile::GetByID($arProperty["VALUE"]);
-			$arFile = $rsFile->Fetch();
-
-//ДЛЯ ИЗМЕНЕНИЯ [ORIGINAL_NAME] на ДАТУ СОЗДАНИЯ ФАЙЛА ----
-			$file_len = strrpos($arFile["ORIGINAL_NAME"], '.');
-			$file_name = substr($arFile["ORIGINAL_NAME"], 0, $file_len);
-			$file_type = substr($arFile["ORIGINAL_NAME"], $file_len+1, strlen($arFile["ORIGINAL_NAME"])-$file_len);
-			$file_name = intval($file_name);
-//ДЛЯ ИЗМЕНЕНИЯ [ORIGINAL_NAME] на ДАТУ СОЗДАНИЯ ФАЙЛА ---- end
-
-			$filename = "/".COption::GetOptionString("main", "upload_dir", "upload")."/".$arFile["SUBDIR"]."/".$arFile["FILE_NAME"];
-
-
-				if($arProperty["DESCRIPTION"])
-				{
-?>
-<div style="padding: 10px 0px 0px 0px; color: black;"><a class="news_date_nav" href="<?=$filename?>" style="font: 10pt/10pt Arial, sans-serif;">
-<?
-					?><?=$arProperty["DESCRIPTION"]?><?
-					if ($DATE_VIEW) {
-						?> (<?=date("d.m.Y",$file_name);?>) <?
-					}
-					?> <nobr><small>(<?=byteCount($arFile["FILE_SIZE"]);?>)</small></nobr><?
-?>
-</a></div>
-<?
-				}else{
-?>
-<li style="padding: 10px 0px 0px 0px; color: black;"><a class="news_date_nav" href="<?=$filename?>" style="font: 10pt/10pt Arial, sans-serif;">
-<?
-					?><?=$arFields["NAME"];?><?
-					if ($DATE_VIEW) {
-						$arDATE = ParseDateTime($arFields["DATE_ACTIVE_FROM"], FORMAT_DATETIME);
-						?> (<?=$arDATE["DD"];?>.<?=$arDATE["MM"];?>.<?=$arDATE["YYYY"];?>) <?
-					}
-					?> <nobr><small>(<?=byteCount($arFile["FILE_SIZE"]);?>)</small></nobr><?
-?>
-</a></li>
-<?
-				}
-		}
-	}
-*/
 $date_active_from_mk = MkDateTime($DB->FormatDate($arFields["ACTIVE_FROM"], Clang::GetDateFormat("SHORT"), "DD.MM.YYYY"), "d.m.Y");
 ?>
-<div class="col-sm-3">
-<p><a href="?IBLOCK_ID=<?=$arParams['IBLOCK_ID'];?>&SECTION_ID=0&ELEMENT_ID=<?=$arFields['ID'];?>" class=\"submenu_l3\"><?=$arFields['NAME'];?></a></p>
-</div>
-<div class="col-sm-3">
-<p><?=$arPropertyD['rmu_ispolnitel']['VALUE'];?></p>
-</div>
-<div class="col-sm-2">
-<p><center><?=date("d.m.Y", $date_active_from_mk);?></center></p>
-</div>
-<div class="col-sm-2">
-<p><center><?=date("d.m.Y", $date_active_from_mk);?> - <?=date("d.m.Y", dateAfterMonth
-	(
-	date("m", $date_active_from_mk),
-	date("d", $date_active_from_mk),
-	date("Y", $date_active_from_mk)
-	));?></center></p>
-</div>
-<div class="col-sm-2">
-<p><center><?=$arPropertyD['rmu_status']['VALUE_ENUM'];?></center></p>
-</div>
+	<div class="col-sm-3 np">
+		<p><a href="?IBLOCK_ID=<?=$arParams['IBLOCK_ID'];?>&SECTION_ID=0&ELEMENT_ID=<?=$arFields['ID'];?>" class=\"submenu_l3\"><?=$arFields['NAME'];?></a></p>
+	</div>
+	<div class="col-sm-3 np">
+		<p><?=$arPropertyD['rmu_ispolnitel']['VALUE'];?></p>
+	</div>
+	<div class="col-sm-2 np">
+		<p><center><?=date("d.m.Y", $date_active_from_mk);?></center></p>
+	</div>
+	<div class="col-sm-2 np">
+		<p><center><?=date("d.m.Y", $date_active_from_mk);?> - <?=date("d.m.Y", dateAfterMonth(date("m", $date_active_from_mk),date("d", $date_active_from_mk),date("Y", $date_active_from_mk)));?></center></p>
+	</div>
+	<div class="col-sm-2 np">
+		<p><center><?=$arPropertyD['rmu_status']['VALUE_ENUM'];?></center></p>
+	</div>
 </div>
 <?
-//<ul type="1"><font class="application" style="font: bold 10pt/10pt Arial,sans-serif;" color="#7d0000">
-
 			}
 		}
 		$l--;
-//</font></ul>
 ?>
 <?
 
